86a507667940ffaff6bc643a663c81b1c8daeeb9
Ajjurock150 <93971996+Ajjurock150@users.noreply.github.com>
2023-09-13 20:48:21 +0530

    Add files via upload

---
+++ 24 array operations.py
@@ -0,0 +1,16 @@
+from numpy import *
+arr1=array([1,2,3,4,5,])
+arr2=array([6,7,8,9,10])
+arr3 = arr1 + arr2
+print(arr3)
+#adding each inedxes in 2 arrays
+#arr1[1]+arr2[1]
+
+#print(sin(arr3))
+#print(log(arr1))
+#print(sqrt(arr1))
+print(sum(arr1))
+print(max(arr1))
+print(min(arr1))
+print(len(arr1))
+print(size(arr3))

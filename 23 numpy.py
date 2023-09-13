ddfc720e4f47b8d597cf4d13105192d8cfb8192c
Ajjurock150 <93971996+Ajjurock150@users.noreply.github.com>
2023-09-13 20:47:59 +0530

    Add files via upload

---
+++ 23 numpy.py
@@ -0,0 +1,25 @@
+
+#1 way
+from numpy import *
+arr1=array([5,6.5,7])
+arr=array([1,2.5,3],int)
+print (arr)
+print(arr1)
+
+#2nd way
+arr2=linspace(1,15,15)
+print (arr2)
+
+#3rd way
+arr3=arange(1,15,2)
+print(arr3)
+
+#4th way
+arr4=ones(5,int)
+print(arr4)
+
+#5th way
+arr5=zeros(6)
+print(arr5)
+
+
